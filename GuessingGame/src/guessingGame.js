e248e3c57fb69228a124d52cbbe2d5f8df7143fd
LissAponte <japonterivera@student.neumont.edu>
2025-07-23 11:34:04 -0600

    updated project

--- GuessingGame/src/guessingGame.js
+++ GuessingGame/src/guessingGame.js
@@ -1,27 +1,7 @@
-const randomNum = Math.floor(Math.random() * 100) + 1;
-console.log("The answer is: " + randomNum);
-let numberOfGuesses = 0;
+"use strict";
 
-function checkGuess(){
-    const guessInput = document.getElementById("guessInput");
-    const feedBack = document.getElementById("FeedBack!");
-    const guess = document.getElementById(guessInput.value);
-
-    numberOfGuesses++;
-document.getElementById('button').addEventListener('click', evt => {
-    let data = document.getElementById('guessInput').value;
-});
-document.getElementById('guessInput').value = "";
-
-    if (isNaN(guess) || guess < 1 || guess > 100) {
-        feedback.textContent = "Invalid input. Please enter a  number between 1 and 100.";
-    } else if (guess < randomNum) {
-        feedBack.textContent = "Too low!";
-    } else if (guess > randomNum){
-        feedBack.textcontext = "Too high!";
-    } else {
-        feedBack.textContext = "Congratulations! You win!";
-        guessInput.disabled = true;
-    }
-}
-
+const containerEL = document.getElementById(".container");
+const playAgainEL = document.getElementById(".play-again");
+const checkGuessEL = document.getElementById(".check-button");
+const hideNumEL = document.getElementById(".hide-num");
+const hideNumEL = document.getElementById(".hide-num");
