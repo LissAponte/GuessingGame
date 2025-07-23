bfccb9dde60c1e02efb1c6200de7727d023ac680
LissAponte <japonterivera@student.neumont.edu>
2025-07-23 11:37:59 -0600

    Update guessingGame.js

--- GuessingGame/src/guessingGame.js
+++ GuessingGame/src/guessingGame.js
@@ -1,7 +1,16 @@
 "use strict";
 
-const containerEL = document.getElementById(".container");
-const playAgainEL = document.getElementById(".play-again");
-const checkGuessEL = document.getElementById(".check-button");
-const hideNumEL = document.getElementById(".hide-num");
-const hideNumEL = document.getElementById(".hide-num");
+//region Const
+const container = document.getElementById(".container");
+const playAgain = document.getElementById(".play-again");
+const checkGuess = document.getElementById(".check-button");
+const hideNum = document.getElementById(".hide-num");
+const message = document.getElementById(".started-message");
+const inputNum = document.getElementById(".input-number");
+const hScore = document.getElementById(".h-score");
+const score = document.getElementById(".score");
+
+//endregion
+
+let answer = Math.random() * 100 + 1;
+console.log(answer);
