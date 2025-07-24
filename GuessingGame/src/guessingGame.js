c4428bc03673e3013028f4f24b4531c4c40ae0c3
LissAponte <japonterivera@student.neumont.edu>
2025-07-23 19:13:19 -0600

    updated

--- GuessingGame/src/guessingGame.js
+++ GuessingGame/src/guessingGame.js
@@ -1,8 +1,7 @@
 "use strict";
 
 //region Const
-const containerEL = document.getElementById(".container");
-const playAgainEL = document.getElementById(".play-again");
+const playAgainEL = document.getElementById("play-again");
 const checkGuessEL = document.getElementById(".check-button");
 const hideNumEL = document.getElementById(".hide-num");
 const messageEL = document.getElementById(".started-message");
@@ -16,8 +15,9 @@ let answer = Math.random() * 100 + 1;
 let score = 20;
 let hScore = 0;
 
+console.log(answer);
 
-checkGuessEL.addEventListener('click',()=>{
+checkGuessEL.addEventListener('click',function(){
     const guess = Number(inputNumEL.value);
 if(guess > answer) {
     messageEL.textContent = "Number is too high! You've lost :<"
@@ -27,6 +27,10 @@ if(guess > answer) {
     scoreEL.textContent = 0
 
 } else if (guess === answer) {
+    hideNumEL.textContent = answer;
+    hideNumEL.style.width = "50%";
+    hideNumEL.style.transition = "aLL 0.3 ease in";
+
     messageEL.textContent = "Correct! You win! :3"
 } else {
     messageEL.textContent = "Please enter a number<3"
